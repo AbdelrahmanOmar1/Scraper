775957527a3ef33b4d77392c83fc671bcfa0f823
Abdelrahman <86893144+AbdelrahmanOmar1@users.noreply.github.com>
2026-06-15 07:58:06 +0300

    Update CORS configuration to allow all origins

--- findlyBackend/app.js
+++ findlyBackend/app.js
@@ -10,9 +10,8 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+    origin: "*", 
+  })
 );
 app.use(morgan("dev"));
 
